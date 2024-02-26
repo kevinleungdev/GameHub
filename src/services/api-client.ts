917d586f1a68c1835bc2e6d4afb29a0bf17d2ecb
Kevin Liang <kevinleungdev@outlook.com>
2024-02-26 10:50:58 +0800

    Refactor- move the interface FetchResponse to the right place as useData hook is no longer to be used

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "3db225743b314b0fa79b911e392be2ea"
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "3db225743b314b0fa79b911e392be2ea",
+  },
+});
